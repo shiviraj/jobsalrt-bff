2266895dae3bd11334017c4809f761504e076e95
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-07-09 08:36:23 +0530

    Added api for updated views.

--- src/services/post.js
+++ src/services/post.js
@@ -4,6 +4,9 @@ const PostService = {
   getPostByUrl(url) {
     return Post.getPostByUrl(url)
   },
+  updateViewsByUrl(url) {
+    return Post.updateViewsByUrl(url)
+  }
 }
 
 export default PostService
