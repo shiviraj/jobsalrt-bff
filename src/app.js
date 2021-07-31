9a4ef3832bdf43385d3f03cc6d2c418144f9463c
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-07-31 12:36:59 +0530

    Enable cors

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 
 app.use(initEncryption)
 
-app.use(cors({origin: process.env.ALLOWED_ORIGIN || "http://localhost:3000"}))
+app.use(cors())
 app.use('/api', getRouter())
 
 app.disable('x-powered-by')
